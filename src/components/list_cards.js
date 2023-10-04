7e0480ffbe48ad89b13bb0e3e759c91976d91d31
igorG7 <giovane167@outlook.com>
2023-10-04 15:49:41 -0300

    Componentização dos Cards

---
+++ src/components/list_cards.js
@@ -0,0 +1,40 @@
+const classColor = ["work", "play", "study", "exercise", "social", "self-care"];
+
+const url = [
+  "src/img/icon-work.svg",
+  "src/img/icon-play.svg",
+  "src/img/icon-study.svg",
+  "src/img/icon-exercise.svg",
+  "src/img/icon-social.svg",
+  "src/img/icon-self-care.svg",
+];
+
+export const listCards = (data) => {
+  return data
+    .map((item, index) => {
+      const mapClass = classColor.map((item) => {
+        return item;
+      });
+
+      const mapUrl = url.map((item) => {
+        return item;
+      });
+      return `
+
+    <div class="card ${mapClass[index]}" > 
+    <img class="icon-card" src="${mapUrl[index]}" alt="" />
+    <div class="info-card">
+      <div class="categ-more">
+        <p class="category"></p>
+        <p class="more">...</p>
+      </div>
+      <div class="period-hours">
+        <p class="hour"></p>
+        <p class="period" id="period"></p>
+      </div>
+    </div>
+    </div>
+    `;
+    })
+    .join("");
+};
